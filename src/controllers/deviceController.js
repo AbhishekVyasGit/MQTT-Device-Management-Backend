d1591bd95a4e49702026aa7e0a6b006811f9c69b
Abhishek <abhishekvyas30008@gmail.com>
2023-11-23 22:01:38 +0530

    add some events in mqttService

--- src/controllers/deviceController.js
+++ src/controllers/deviceController.js
@@ -1,6 +1,4 @@
 
-const express = require('express');
-const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 const Device = require('../models/deviceModel');
 const mqttService = require('../services/mqttService');
--- src/services/mqttService.js
+++ src/services/mqttService.js
@@ -9,6 +9,28 @@ client.on('connect', () => {
   console.log('Connected to MQTT broker');
 });
 
+client.on('message', (topic, message) => {
+  console.log(`Received message on topic: ${topic.toString()} and message: ${message.toString()}`);
+});
+
+client.on('offline', () => {
+  console.log('Client is offline');
+});
+
+client.on('reconnect', () => {
+  console.log('Reconnecting to MQTT broker');
+});
+
+client.on('end', () => { 
+  console.log('Connection to MQTT broker ended');
+});
+
+
+client.on('error', (err) => {
+  console.error('Error connecting to MQTT broker:', err);
+});
+
+
 function subscribe(topic, callback) {
   client.subscribe(topic, callback);
 }
